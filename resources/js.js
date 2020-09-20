0c1aa7588bbf795c7437386ae7b8eaccf93a1a58
RamiNawahda <rami.nawahda@gmail.com>
2020-09-20 17:16:04 +0200

    third commit

--- resources/js.js
+++ resources/js.js
@@ -1,18 +1,34 @@
-
-	var questionBank=[];
+var questionBank=[];
 	
 function createQuestions(questionBank){
 $("#csvFile").attr("style","display:none;");
 			for(var i=0, clas="";i<questionBank.length;i++)
 			{
 							
-				if(i>0)  clas="class='hide'";
-				var div="<div "+clas+">	<table><tbody><tr><th> <h1><center>"+questionBank[i]['q']+"</center></h1></th></tr><tr><td><input type='radio' name='a"+i+"'>"+questionBank[i]['a1']+"<br><input type='radio' name='a"+i+"'>"+questionBank[i]['a2']+"<br><input type='radio' name='a"+i+"'>"+questionBank[i]['a3']+"<br><input type='radio' name='a"+i+"'>"+questionBank[i]['a4']+"<br></td></tr></tbody></table></div>";
+				if(i>0){ 
+				clas="class='hide'";
+				
+				
+				}
+				var div="<div id='q"+i+"' "+clas+">	<table><tbody><tr><th> <h1><center>"+questionBank[i]['q']+"</center></h1></th></tr><tr><td><input type='radio' name='a"+i+"' id='a"+i+"' value='1'>"+questionBank[i]['a1']+"<br><input type='radio' name='a"+i+"'  id='a"+i+"' value='2'>"+questionBank[i]['a2']+"<br><input type='radio' name='a"+i+"'  id='a"+i+"' value='3'>"+questionBank[i]['a3']+"<br><input type='radio' name='a"+i+"' id='a"+i+"' value='4'>"+questionBank[i]['a4']+"<br></td></tr><tr id='prevNext'><td><button id='"+i+"' onclick='showHide(this.id)'>Next</button></td></tr></tbody></table></div>";
 				$("#questionsContainer").append(div);
 				//$("<div "+clas+">"+questionBank[i]['q']+"</div>").appendTo($("#questionsContainer"))
 				}
 }
-			
+var userAnswers=[];
+function showHide(id){
+	//console.log($("#a"+id+":checked").val(),"#a"+id)
+	//console.log(Number($("#a"+id+":checked").val())==Number(questionBank[id]['ans']));
+	if(Number($("#a"+id+":checked").val())==Number(questionBank[id]['ans']))userAnswers[id]={qNo:id,answer:1};
+	else userAnswers[id]={qNo:id,answer:0};
+$("#q"+id).attr("class","hide");
+id=Number(id);
+id++;
+​
+$("#q"+id).attr("class","");
+​
+​
+}	
 $(document).ready(function() { 
 		
 	$("#csvFile").change(function() { 
@@ -42,6 +58,6 @@ $(document).ready(function() {
 			} 
 			
 			csvReader.readAsText(this.files[0]); 
-
+​
 	}); 
 }); 
