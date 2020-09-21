e0d39b249a680b0d968329ef8d18a40b87ce1f48
RamiNawahda <rami.nawahda@gmail.com>
2020-09-21 09:24:55 +0200

    morning commit

--- resources/js.js
+++ resources/js.js
@@ -1,52 +1,66 @@
-var questionBank=[];
+
+	var questionBank=[];
 	
 function createQuestions(questionBank){
 $("#csvFile").attr("style","display:none;");
+			questionBank = questionBank.sort(() => Math.random() - 0.5)
 			for(var i=0, clas="";i<questionBank.length;i++)
 			{
 							
 				if(i>0){ 
 				clas="class='hide'";
-				
-				
 				}
-				var div="<div id='q"+i+"' "+clas+">	<table><tbody><tr><th> <h1><center>"+questionBank[i]['q']+"</center></h1></th></tr><tr><td><input type='radio' name='a"+i+"' id='a"+i+"' value='1' onclick=showHideNext("+i+")>"+questionBank[i]['a1']+"<br><input type='radio' name='a"+i+"'  id='a"+i+"' value='2' onclick=showHideNext("+i+")>"+questionBank[i]['a2']+"<br><input type='radio' name='a"+i+"'  id='a"+i+"' value='3' onclick=showHideNext("+i+")>"+questionBank[i]['a3']+"<br><input type='radio' name='a"+i+"' id='a"+i+"' value='4' onclick=showHideNext("+i+")>"+questionBank[i]['a4']+"<br></td></tr><tr id='prevNext'><td><button class='hide' id='"+i+"' onclick='showHide(this.id)'>Next</button></td></tr></tbody></table></div>";
+				$("#id0").attr("class","hide");
+				var div="<div id='q"+i+"' "+clas+">	<table class='bigFont'><tbody><tr><th> <h1><center>"+questionBank[i]['q']+"</center></h1></th></tr><tr><td><input type='radio' name='a"+i+"' id='a"+i+"' value='1' onclick=showHideNext("+i+")>"+questionBank[i]['a1']+"<br><input type='radio' name='a"+i+"'  id='a"+i+"' value='2' onclick=showHideNext("+i+")>"+questionBank[i]['a2']+"<br><input type='radio' name='a"+i+"'  id='a"+i+"' value='3' onclick=showHideNext("+i+")>"+questionBank[i]['a3']+"<br><input type='radio' name='a"+i+"' id='a"+i+"' value='4' onclick=showHideNext("+i+")>"+questionBank[i]['a4']+"<br></td></tr><tr id='prevNext'><td><button class='hide' id='"+i+"' onclick='showHide(this.id,1)'>Next</button><button class='' id='id"+i+"' onclick='showHide(this.id)'>Previous</button></td></tr></tbody></table></div>";
 				$("#questionsContainer").append(div);
 				//$("<div "+clas+">"+questionBank[i]['q']+"</div>").appendTo($("#questionsContainer"))
 				}
 
 }
-var userAnswers=[];
-function showHide(id){
-	
+var usermarks=[];
+function showHide(id,a){
+	if(a!=1)id=id[2];
+	//console.log(id);
 	//console.log($("#a"+id+":checked").val(),"#a"+id)
 	//console.log(Number($("#a"+id+":checked").val())==Number(questionBank[id]['ans']));
-	if(Number($("#a"+id+":checked").val())==Number(questionBank[id]['ans']))userAnswers[id]={qNo:id,answer:1};
-	else userAnswers[id]={qNo:id,answer:0};
+	if(Number($("#a"+id+":checked").val())==Number(questionBank[id]['ans']))var score=1;
+	else score=0;
+	usermarks[id]={qNo:id,mark:score,userAnswer:$("#a"+id+":checked").val(),trueAnswer:Number(questionBank[id]['ans'])};
 $("#q"+id).attr("class","hide");
 id=Number(id);
+if(a===1){
 id++;
 if(Number(id)===questionBank.length){
 	
-					var sum=0;
-				for(j=0;j<userAnswers.length;j++){ sum+=userAnswers[j]['answer']; }
-				if(sum>(j/2)) var result="You pass with "+sum+" answer out of "+j;
-				else  var result="You fail with "+sum+" answer out of "+j;
-				var end="<div >"+result+"</div>"
+					var sum=0;t="<table class='bigFont' border='1'><tr><th>Question</th><th>Answer Check</th><th>Mark</th></tr>";
+				for(j=0;j<usermarks.length;j++){
+					sum+=usermarks[j]['mark'];
+					if(questionBank[j]['a'+usermarks[j]['userAnswer']]===questionBank[j]['a'+usermarks[j]['trueAnswer']])var assess="Your Answer <b>"+questionBank[j]['a'+usermarks[j]['userAnswer']]+"</b> is True";
+					else var assess="Your Answer <b>"+questionBank[j]['a'+usermarks[j]['userAnswer']]+"</b> is False and the True one is: <br><b>"+questionBank[j]['a'+usermarks[j]['trueAnswer']]+"</b>";
+					t+="<tr><td>"+questionBank[j]['q']+"</td><td>"+assess+"</td><td>"+usermarks[j]['mark']+"</td></tr>";
+				}
+				t+="</table>";
+				if(sum>(j/2)) var result="You pass with "+sum+" mark out of "+j;
+				else  var result="You fail with "+sum+" mark out of "+j;
+				var end="<div class='bigFont' >"+t+""+result+"</div>"
 				$("#questionsContainer").append(end);
 }
+}
+else id--;
 $("#q"+id).attr("class","");
 }	
 function showHideNext(id){
 	$("#"+id).attr("class","");
 }
 $(document).ready(function() { 
-
+/// this link on how you get file content https://www.geeksforgeeks.org/how-to-read-a-local-text-file-using-javascript/ && https://javascript.info/file we apply it in our project
 	$("#csvFile").change(function() { 
 					var csvReader=new FileReader(); 
+					csvReader.readAsText(this.files[0]); 
 			csvReader.onload=function(){ 
 		var arrayOfQuestions=csvReader.result.split("\n"); 
 		//console.log(csvReader.result.split("\n"));
+		//for-loop to iterate over data after we split it using new line then we split each line to object inside for-loop 
 			for( var i=1;i< arrayOfQuestions.length;i++){
 				/***** you have to use if line has data (arr.length**/
 				if(arrayOfQuestions[i].split(";").length===6){
@@ -65,10 +79,11 @@ $(document).ready(function() {
 				}
 						 
 			}
+			// function to Create Questions DIVs
 				createQuestions(questionBank);		
 			} 
 			
-			csvReader.readAsText(this.files[0]); 
+			
 
 	}); 
 }); 
